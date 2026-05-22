615e0b88078eaf5a5a0da0d1b1fd55d868adf0ee
engbede <emmanuelngbede74@gmail.com>
2026-05-22 16:24:44 +0100

    my very first commit

---
+++ CountAlha.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func CountAlpha(s string) int {
+	count := 0
+
+	for i := 0; i < len(s); i++ {
+		if (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H1e2l3l4o"))
+}
